0637edde64c6bcecd224dcaf8df639eb0ae21053
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2021-06-12 13:48:52 +0300

    censored mse

--- censored_mae_mse/loss.py
+++ censored_mae_mse/loss.py
@@ -1,6 +1,10 @@
 import torch as t
 import math
 
+'''
+Censored MAE
+'''
+
 mae = t.nn.L1Loss()
 
 def truncation_mae_penalty(y_pred, lower_truncation_limit = None, upper_truncation_limit = None):
@@ -13,7 +17,7 @@ def truncation_mae_penalty(y_pred, lower_truncation_limit = None, upper_truncati
     upper_truncation_penalty = mae(above_upper_truncation_limit, t.full_like(above_upper_truncation_limit, upper_truncation_limit))
   return lower_truncation_penalty + upper_truncation_penalty
 
-def censored_mae(y_pred, y, lower_censoring_bound = -math.inf, upper_censoring_bound = math.inf, device = 'cpu', clamp_all = False):
+def censored_mae(y_pred, y, lower_censoring_bound = -math.inf, upper_censoring_bound = math.inf, device = 'cpu', clamp_all = True):
   if clamp_all:
     y_pred = t.clamp(y_pred, min = lower_censoring_bound, max = upper_censoring_bound)
   else:
@@ -29,10 +33,45 @@ def censored_mae_with_truncation_penalty(
         lower_truncation_limit = None,
         upper_truncation_limit = None,
         device = 'cpu',
-        clamp_all = False
+        clamp_all = True
   ):
   return censored_mae(y_pred, y, lower_censoring_bound = lower_censoring_bound, upper_censoring_bound = upper_censoring_bound, device = device, clamp_all = clamp_all) \
          + truncation_mae_penalty(y_pred, lower_truncation_limit = lower_truncation_limit, upper_truncation_limit = upper_truncation_limit)
 
+'''
+Censored MSE
+'''
 
+mse = t.nn.MSELoss()
+
+def truncation_mse_penalty(y_pred, lower_truncation_limit = None, upper_truncation_limit = None):
+  lower_truncation_penalty, upper_truncation_penalty = 0, 0
+  if lower_truncation_limit:
+    below_lower_truncation_limit = t.clamp(y_pred, min = -math.inf, max = lower_truncation_limit)
+    lower_truncation_penalty = mse(below_lower_truncation_limit, t.full_like(below_lower_truncation_limit, lower_truncation_limit))
+  if upper_truncation_limit:
+    above_upper_truncation_limit = t.clamp(y_pred, min = upper_truncation_limit, max = math.inf)
+    upper_truncation_penalty = mse(above_upper_truncation_limit, t.full_like(above_upper_truncation_limit, upper_truncation_limit))
+  return lower_truncation_penalty + upper_truncation_penalty
+
+def censored_mse(y_pred, y, lower_censoring_bound = -math.inf, upper_censoring_bound = math.inf, device = 'cpu', clamp_all = False):
+  if clamp_all:
+    y_pred = t.clamp(y_pred, min = lower_censoring_bound, max = upper_censoring_bound)
+  else:
+    y_pred = t.where((y > lower_censoring_bound) + (y_pred > lower_censoring_bound), y_pred, t.tensor(lower_censoring_bound, device = device))
+    y_pred = t.where((y < upper_censoring_bound) + (y_pred < upper_censoring_bound), y_pred, t.tensor(upper_censoring_bound, device = device))
+  return mse(y_pred, y)
+
+def censored_mse_with_truncation_penalty(
+        y_pred,
+        y,
+        lower_censoring_bound = -math.inf,
+        upper_censoring_bound = math.inf,
+        lower_truncation_limit = None,
+        upper_truncation_limit = None,
+        device = 'cpu',
+        clamp_all = False
+  ):
+  return censored_mse(y_pred, y, lower_censoring_bound = lower_censoring_bound, upper_censoring_bound = upper_censoring_bound, device = device, clamp_all = clamp_all) \
+         + truncation_mse_penalty(y_pred, lower_truncation_limit = lower_truncation_limit, upper_truncation_limit = upper_truncation_limit)
 
--- censored_mae_mse/test/censored_mse_with_truncation_clamp_all_test.py
+++ censored_mae_mse/test/censored_mse_with_truncation_clamp_all_test.py
@@ -1,62 +1,62 @@
 import unittest
 import torch as t
-from censored_mae_mse.loss import censored_mae_with_truncation_penalty
+from censored_mae_mse.loss import censored_mse_with_truncation_penalty
 
 class TobitOptimizationTest(unittest.TestCase):
 
     def test_uncensored(self):
         y = t.tensor([1.2, 1.5, 2])
         y_pred = t.tensor([1.3, 1.7, 2.3])
-        expected_loss = t.tensor(.2)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, clamp_all = False)
+        expected_loss = t.tensor((.01 + .04 + .09) / 3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_lower_censored_1(self):
         low_censor = 1.4
         y = t.tensor([low_censor, 1.5, 2])
         y_pred = t.tensor([1.3, 1.7, 2.3])
-        expected_loss = t.tensor((.2 + .3)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = False)
+        expected_loss = t.tensor((.04 + .09)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_lower_censored_2(self):
         low_censor = 1.6
         y = t.tensor([low_censor, low_censor, 2])
         y_pred = t.tensor([1.3, 1.7, 2.3])
-        expected_loss = t.tensor((.1 + .3)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = False)
+        expected_loss = t.tensor((.01 + .09)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_lower_censored_3(self):
         low_censor = 1.6
         y = t.tensor([low_censor, 1.7, 2])
         y_pred = t.tensor([1.3, 1.0, 2.3])
-        expected_loss = t.tensor((.7 + .3)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = False)
+        expected_loss = t.tensor((.01 + .09)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = low_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_upper_censored_1(self):
         upper_censor = 1.6
         y = t.tensor([1.2, 1.5, upper_censor])
         y_pred = t.tensor([1.3, 1.7, 2.3])
-        expected_loss = t.tensor((.1 + .2)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = False)
+        expected_loss = t.tensor((.01 + .01)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_upper_censored_2(self):
         upper_censor = 1.6
         y = t.tensor([1.2, upper_censor, upper_censor])
         y_pred = t.tensor([1.3, 1.5, 2.3])
-        expected_loss = t.tensor((.1 + .1)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = False)
+        expected_loss = t.tensor((.01 + .01)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_upper_censored_3(self):
         upper_censor = 1.6
         y = t.tensor([1.2, 1.5, upper_censor])
         y_pred = t.tensor([1.3, 2.0, 2.3])
-        expected_loss = t.tensor((.1 + .5)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = False)
+        expected_loss = t.tensor((.01 + .01)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, upper_censoring_bound = upper_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_combined_1(self):
@@ -64,32 +64,32 @@ class TobitOptimizationTest(unittest.TestCase):
         upper_censor = 1.7
         y = t.tensor([lower_censor, lower_censor, 1.4, 1.5, upper_censor, upper_censor])
         y_pred = t.tensor([1.0, 1.4, 1.6, 2.0, 1.6, 1.8])
-        expected_loss = t.tensor((0 + .1 + .2 + .5 + .1 + 0)/6)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = lower_censor, upper_censoring_bound = upper_censor, clamp_all = False)
+        expected_loss = t.tensor((0 + .01 + .04 + .04 + .01 + 0)/6)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y, lower_censoring_bound = lower_censor, upper_censoring_bound = upper_censor, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_truncated_1(self):
         lower_truncation = 1.
         y = t.tensor([1.1, 1.2, 1.3])
         y_pred = t.tensor([.9, 1.1, 1.3])
-        expected_loss = t.tensor((0.3 + 0.1 + 0)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred=y_pred, y=y, lower_truncation_limit = lower_truncation, clamp_all = False)
+        expected_loss = t.tensor((.04 + .01 + .01 + 0)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred=y_pred, y=y, lower_truncation_limit = lower_truncation, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_truncated_2(self):
         lower_truncation = 1.
         y = t.tensor([1.1, 1.2, 1.3])
         y_pred = t.tensor([.9, .9, 1.3])
-        expected_loss = t.tensor((0.3 + 0.4 + 0)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred=y_pred, y=y, lower_truncation_limit = lower_truncation, clamp_all = False)
+        expected_loss = t.tensor((.01 + .04 + .01 + 0.09 + 0)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred=y_pred, y=y, lower_truncation_limit = lower_truncation, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_truncated_3(self):
         upper_truncation = 1.
         y = t.tensor([.7, .8, .9])
         y_pred = t.tensor([.7, .9, 1.2])
-        expected_loss = t.tensor((0 + 0.1 + 0.5)/3)
-        loss = censored_mae_with_truncation_penalty(y_pred=y_pred, y=y, upper_truncation_limit = upper_truncation, clamp_all = False)
+        expected_loss = t.tensor((0 + 0.01 + 0.09 + 0.04)/3)
+        loss = censored_mse_with_truncation_penalty(y_pred=y_pred, y=y, upper_truncation_limit = upper_truncation, clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
 
     def test_combined_2(self):
@@ -99,7 +99,9 @@ class TobitOptimizationTest(unittest.TestCase):
         upper_truncation = 1.8
         y = t.tensor([lower_censor, lower_censor, 1.4, 1.5, upper_censor, upper_censor])
         y_pred = t.tensor([1.0, 1.4, 1.6, 2.0, 1.6, 1.9])
-        expected_loss = t.tensor((.1 + .1 + .2 + .7 + .1 + .1)/6)
-        loss = censored_mae_with_truncation_penalty(y_pred = y_pred, y = y,
-            lower_censoring_bound = lower_censor, upper_censoring_bound = upper_censor, lower_truncation_limit = lower_truncation, upper_truncation_limit = upper_truncation, clamp_all = False)
+        expected_loss = t.tensor((.01 + .01 + .04 + .04 + .04 + .01 + .01)/6)
+        loss = censored_mse_with_truncation_penalty(y_pred = y_pred, y = y,
+            lower_censoring_bound = lower_censor, upper_censoring_bound = upper_censor,
+            lower_truncation_limit = lower_truncation, upper_truncation_limit = upper_truncation,
+            clamp_all = True)
         self.assertTrue(t.allclose(loss, expected_loss))
