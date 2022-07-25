c84c7d8c6d23abfcccab4beb694769dfcda81a47
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-07-25 18:14:20 +0300

    train tobit

--- experiments/synthetic/train.py
+++ experiments/synthetic/train.py
@@ -115,3 +115,62 @@ def train_network_mae_mse_gll(bound_min, bound_max, model, loss_fn, optimizer, s
         return metrics_train_per_epochs, metrics_test_per_epochs, best
     except KeyboardInterrupt as e:
         print('Training interrupted at epoch', epoch)
+
+def train_network_tobit(bound_min, bound_max, model, loss_fn, optimizer, scheduler, loader_train, loader_val, checkpoint_name, batch_size_train, batch_size_val, epochs, log = True):
+    metrics_train_per_epochs, metrics_test_per_epochs = [], []
+    best = [math.inf, math.inf, -math.inf]
+    try:
+        counter = 0
+        total_weight = 0
+        train_metrics = np.zeros(3)
+        for epoch in range(epochs):
+            try:
+                model.train()
+                for x, y, single_valued_indexes, left_censored_indexes, right_censored_indexes in loader_train:
+                    counter += 1
+                    y_single_valued = y[single_valued_indexes]
+                    y_left_censored = y[left_censored_indexes]
+                    y_right_censored = y[right_censored_indexes]
+                    y_tuple = y_single_valued, y_left_censored, y_right_censored
+                    y_pred = model.forward(x)
+                    y_pred_single_valued = y_pred[single_valued_indexes]
+                    y_pred_left_censored = y_pred[left_censored_indexes]
+                    y_pred_right_censored = y_pred[right_censored_indexes]
+                    y_pred_tuple = y_pred_single_valued, y_pred_left_censored, y_pred_right_censored
+                    loss = loss_fn(y_pred_tuple, y_tuple)
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    y_pred = t.clamp(y_pred, min = bound_min, max = bound_max)
+                    y_pred, y = to_numpy(y_pred), to_numpy(y)
+                    weight = len(y) / batch_size_train
+                    train_metrics[LOSS] += (loss.item() * weight)
+                    train_metrics[ABS_ERR] += (sk.metrics.mean_absolute_error(y, y_pred) * weight)
+                    train_metrics[R_SQUARED] += (adjusted_R2(y, y_pred) * weight)
+                    total_weight += weight
+                    scheduler.step()
+                    if counter % CHECKPOINT_FREQUENCY == 0:
+                        train_metrics /= total_weight
+                        metrics_train_per_epochs.append(train_metrics)
+                        train_metrics = np.zeros(3)
+                        total_weight = 0
+                        test_metrics = eval_network_tobit(bound_min, bound_max, model, loader_val, loss_fn, batch_size_val)
+                        metrics_test_per_epochs.append(test_metrics)
+                        # if test_metrics[R_SQUARED] > best[R_SQUARED]:
+                        if test_metrics[ABS_ERR] < best[ABS_ERR]:
+                            best = test_metrics
+                            checkpoint_dict = {'model': model.state_dict()}
+                            if hasattr(loss_fn, 'gamma'):
+                                checkpoint_dict['gamma'] = loss_fn.gamma
+                            if hasattr(loss_fn, 'sigma'):
+                                checkpoint_dict['sigma'] = loss_fn.sigma
+                            t.save(checkpoint_dict, '{}.tar'.format(checkpoint_name))
+                        if log:
+                            print('Iteration {} abs err {} R2 {}'.format(counter, test_metrics[ABS_ERR], test_metrics[R_SQUARED]))
+            except Exception as e:
+                print(traceback.format_exc())
+                break
+        print('Best absolute error:', best[ABS_ERR], 'R2:', best[R_SQUARED])
+        return metrics_train_per_epochs, metrics_test_per_epochs, best
+    except KeyboardInterrupt as e:
+        print('Training interrupted at epoch', epoch)
