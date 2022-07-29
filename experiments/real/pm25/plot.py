358a3e5d3b8cc052b3d0c53e307341758ad68755
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-07-29 15:24:41 +0300

    refactor

---
+++ experiments/real/pm25/plot.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from experiments.util import LOSS, ABS_ERR, R_SQUARED
+from experiments.real.pm25.dataset import extract_features, pca, bound_min, bound_max, zero_normalized, \
+    CENSOR_LOW_BOUND, CENSOR_HIGH_BOUND, PM_2_5_Dataset
+
+
+def plot_full_dataset(df: pd.DataFrame, size = 0.01, label = None, censored = False):
+    if censored:
+        x, y = extract_features(df, lower_bound = CENSOR_LOW_BOUND, upper_bound = CENSOR_HIGH_BOUND)
+    else:
+        x, y = extract_features(df)
+    x = pca(x)
+    if not censored:
+        min_max = [min(x), max(x)]
+        plt.plot(min_max, [bound_min] * 2, color = 'red')
+        plt.plot(min_max, [bound_max] * 2, color = 'red')
+        plt.plot(min_max, [zero_normalized] * 2, color = 'red')
+    plt.scatter(x, y, s = size, label = label)
+    plt.xlabel('unidimensional PCA')
+    plt.ylabel('PM2.5 (standardized)')
+    # plt.savefig(label + '.pdf', dpi = 300, format = 'pdf')
+
+def plot_train_test(train, test, title, y_title):
+    plt.plot(range(len(train)), train, label = 'Train')
+    plt.plot(range(len(test)), test, label = 'Test')
+    plt.xlabel('Epochs')
+    plt.ylabel(y_title)
+    plt.title(title)
+    plt.legend()
+    # plt.savefig(title + '.png', dpi = 300, format = 'png')
+    plt.show()
+
+def plot_epochs(train_metrics_list, test_metrics_list):
+    test_r2 = list(map(lambda m: m[R_SQUARED], test_metrics_list))
+    test_err = list(map(lambda m: m[ABS_ERR], test_metrics_list))
+    test_loss = list(map(lambda m: m[LOSS], test_metrics_list))
+    train_r2 = list(map(lambda m: m[R_SQUARED], train_metrics_list))
+    train_err = list(map(lambda m: m[ABS_ERR], train_metrics_list))
+    train_loss = list(map(lambda m: m[LOSS], train_metrics_list))
+    plot_train_test(train_loss, test_loss, 'Loss', 'Loss')
+    plot_train_test(train_err, test_err, 'Absolute error', 'Absolute error')
+    plot_train_test(train_r2, test_r2, 'R squared', 'R squared')
+
+def plot_net(model, df: pd.DataFrame, label = 'model prediction', with_std = False):
+    model.eval()
+    x, y_real = extract_features(df)
+    x_pca = pca(x)
+    dataset = PM_2_5_Dataset(x, y_real)
+    y_list = []
+    for i in range(len(dataset)):
+        x, _ = dataset[i]
+        y_pred = model.forward(x.reshape(1, -1))
+        y_list.append(y_pred[0].item())
+    plt.scatter(x_pca, y_list, s = .1, label = label)
--- experiments/util.py
+++ experiments/util.py
@@ -4,6 +4,10 @@ import random
 
 CUDA = t.cuda.is_available()
 
+LOSS = 0
+ABS_ERR = 1
+R_SQUARED = 2
+
 """Reproducible experiments"""
 def set_random_seed():
     SEED = 0
