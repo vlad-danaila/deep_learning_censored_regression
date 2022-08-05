cecce1818a11967627bbba900bdd189c39d206f3
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-08-05 23:16:34 +0300

    example to merge plots on the same row

--- experiments/plot_datasets.py
+++ experiments/plot_datasets.py
@@ -38,3 +38,31 @@ plt.tight_layout()
 
 save_figures('experiments/real/real_datasets_img/real_datasets')
 # plt.close()
+
+# EXAMPLE FOR MERGING PLOTS ON THE SAME ROW
+# import matplotlib.gridspec as gridspec
+# gs = gridspec.GridSpec(2, 4)
+# gs.update(wspace=0.5)
+# ax1 = plt.subplot(gs[0, :2], )
+# ax2 = plt.subplot(gs[0, 2:])
+# ax3 = plt.subplot(gs[1, 1:3])
+#
+# plt.axes(ax1)
+# plt.title('(a) PM2.5 Training Data Set')
+# plot_pm25(train_df_pm25(df_pm25), label ='training data', censored = True)
+# plt.ylim(-2, 7)
+# plt.xlim(-4, 5)
+#
+# plt.axes(ax2)
+# plt.title('(b) PM2.5 Testing Data Set')
+# plot_pm25(test_df_pm25(df_pm25), label ='testing data', show_bounds = False)
+# plt.ylim(-2, 7)
+# plt.xlim(-4, 5)
+#
+# plt.axes(ax3)
+# plt.title('(c) Seoul Bike-Sharing Training Data Set')
+# plot_bike(train_df_bike(df_bike), label ='training data', censored = True)
+# plt.ylim(-2, 7)
+# plt.xlim(-5, 5)
+#
+# plt.show()
