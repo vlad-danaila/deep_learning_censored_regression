eb12c37ae35a57331ccd5d01d73b7ba6485e3bf0
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-08-08 22:50:09 +0300

    composed plot for synthetic cn tobit fixed std

--- experiments/plot/composed/plot_composed_synthetic_cn_tobit_fixed.py
+++ experiments/plot/composed/plot_composed_synthetic_cn_tobit_fixed.py
@@ -10,56 +10,41 @@ from experiments.synthetic.constant_noise.tobit_based.scaled_fixed_std.scaled_gl
 
 def plot_composed_synthetic_cn_tobit_fixed_std():
     plt.rcParams.update({'font.size': PLOT_FONT_SIZE})
+    fig = plt.figure()
+    fig.set_size_inches(6, 6)
 
-    gs = gridspec.GridSpec(2, 4)
+    gs = gridspec.GridSpec(3, 4, figure=fig)
     gs.update(wspace=.5)
-    gs.update(hspace=.3)
-    ax1 = plt.subplot(gs[0, 1:3])
-    ax2 = plt.subplot(gs[1, :2], )
-    ax3 = plt.subplot(gs[1, 2:])
+    gs.update(hspace=.5)
 
+    ax1 = plt.subplot(gs[0, 1:3])
+    ax2 = plt.subplot(gs[1, :2])
+    ax3 = plt.subplot(gs[1, 2:])
+    ax4 = plt.subplot(gs[2, :2])
+    ax5 = plt.subplot(gs[2, 2:])
 
     plt.axes(ax1)
-    # plt.title('(a) PM2.5 Training Data Set')
     plot_deep_tobit_WITH_trunc()
-    # plt.ylim(-2, 7)
-    # plt.xlim(-4, 5)
+    plt.title('(a) Tobit Log-Likelihood With Truncation')
 
     plt.axes(ax2)
     plot_deep_tobit_NO_trunc()
+    plt.title('(b) Tobit Log-Likelihood (No Truncation)')
 
     plt.axes(ax3)
     plot_gll_scaled()
+    plt.title('(c) Log-Likelihood (No Censoring, No Truncation)')
+
+    plt.axes(ax4)
+    plot_linear_tobit_WITH_trunc()
+    plt.title('(d) Linear Tobit Log-Likelihood With Truncation')
+
+    plt.axes(ax5)
+    plot_linear_tobit_NO_trunc()
+    plt.title('(e) Linear Tobit Log-Likelihood (No Truncation)')
 
     plt.show()
     save_figures('experiments/all_img/synt_cn_tobit_fixed_std')
 
-plot_composed_synthetic_cn_tobit_fixed_std()
 
-# EXAMPLE FOR MERGING PLOTS ON THE SAME ROW
-# import matplotlib.gridspec as gridspec
-# gs = gridspec.GridSpec(2, 4)
-# gs.update(wspace=0.5)
-# ax1 = plt.subplot(gs[0, :2], )
-# ax2 = plt.subplot(gs[0, 2:])
-# ax3 = plt.subplot(gs[1, 1:3])
-#
-# plt.axes(ax1)
-# plt.title('(a) PM2.5 Training Data Set')
-# plot_pm25(train_df_pm25(df_pm25), label ='training data', censored = True)
-# plt.ylim(-2, 7)
-# plt.xlim(-4, 5)
-#
-# plt.axes(ax2)
-# plt.title('(b) PM2.5 Testing Data Set')
-# plot_pm25(test_df_pm25(df_pm25), label ='testing data', show_bounds = False)
-# plt.ylim(-2, 7)
-# plt.xlim(-4, 5)
-#
-# plt.axes(ax3)
-# plt.title('(c) Seoul Bike-Sharing Training Data Set')
-# plot_bike(train_df_bike(df_bike), label ='training data', censored = True)
-# plt.ylim(-2, 7)
-# plt.xlim(-5, 5)
-#
-# plt.show()
+plot_composed_synthetic_cn_tobit_fixed_std()
