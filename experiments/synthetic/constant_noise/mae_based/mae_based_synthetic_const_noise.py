43066a4c920249fb4bb486ed5f7e09de58af9e7e
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-08-15 23:59:18 +0300

    fix

--- experiments/synthetic/constant_noise/mae_based/mae_based_synthetic_const_noise.py
+++ experiments/synthetic/constant_noise/mae_based/mae_based_synthetic_const_noise.py
@@ -43,12 +43,12 @@ zero_normalized = normalize(0, y_mean, y_std)
 
 """# MAE"""
 
-objective_mae_mse = get_objective_fn_mae_mse(
+objective_mae_simple = get_objective_fn_mae_mse(
     dataset_train, dataset_val, bound_min, bound_max, f'{ROOT_MAE}/{CHECKPOINT_MAE}', t.nn.L1Loss, plot = False, log = False)
 
 """TPE Hyperparameter Optimisation"""
 def tpe_opt_mae_simple():
-    best = tpe_opt_hyperparam(ROOT_MAE, CHECKPOINT_MAE, objective_mae_mse)
+    best = tpe_opt_hyperparam(ROOT_MAE, CHECKPOINT_MAE, objective_mae_simple)
     return best
 
 def eval_mae_simple():
@@ -73,11 +73,12 @@ def bounded_loss(y_pred, y):
   y_pred = t.clamp(y_pred, min = bound_min, max = bound_max)
   return mae(y_pred, y)
 
-train_and_evaluate_net = get_objective_fn_mae_mse(
+objective_mae_bounded = get_objective_fn_mae_mse(
     dataset_train, dataset_val, bound_min, bound_max, ROOT_BOUNDED_MAE + '/' + CHECKPOINT_BOUNDED_MAE, lambda: bounded_loss, plot = False, log = False)
 
 def tpe_opt_mae_cens_NO_trunc():
-    return tpe_opt_hyperparam(ROOT_BOUNDED_MAE, CHECKPOINT_BOUNDED_MAE, train_and_evaluate_net)
+    best = tpe_opt_hyperparam(ROOT_BOUNDED_MAE, CHECKPOINT_BOUNDED_MAE, objective_mae_bounded)
+    return best
 
 
 def eval_mae_cens_NO_trunc():
@@ -101,13 +102,13 @@ def below_zero_mae_penalty(y_pred):
 def bounded_loss_with_penalty(y_pred, y):
   return bounded_loss(y_pred, y) + below_zero_mae_penalty(y_pred)
 
-train_and_evaluate_net = get_objective_fn_mae_mse(
+objective_mae_bounded_pen = get_objective_fn_mae_mse(
     dataset_train, dataset_val, bound_min, bound_max,
     ROOT_BOUNDED_MAE_WITH_PENALTY + '/' + CHECKPOINT_BOUNDED_MAE_WITH_PENALTY,
     lambda: bounded_loss_with_penalty, plot = False, log = False)
 
 def tpe_opt_mae_cens_WITH_trunc():
-    return tpe_opt_hyperparam(ROOT_BOUNDED_MAE_WITH_PENALTY, CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, train_and_evaluate_net)
+    return tpe_opt_hyperparam(ROOT_BOUNDED_MAE_WITH_PENALTY, CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, objective_mae_bounded_pen)
 
 def eval_mae_cens_WITH_trunc():
     plot_and_evaluate_model_mae_mse(bound_min, bound_max, x_mean, x_std, y_mean, y_std, dataset_val, dataset_test, ROOT_BOUNDED_MAE_WITH_PENALTY,
@@ -118,3 +119,11 @@ def plot_mae_cens_WITH_trunc():
     plot_dataset_and_net(checkpoint, DenseNetwork(), x_mean, x_std, y_mean, y_std, dataset_val)
 
 
+tpe_opt_mae_simple()
+eval_mae_simple()
+
+tpe_opt_mae_cens_NO_trunc()
+eval_mae_cens_NO_trunc()
+
+tpe_opt_mae_cens_WITH_trunc()
+eval_mae_cens_WITH_trunc()
