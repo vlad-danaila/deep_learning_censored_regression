f7156a5c7604a1e6083d676dd10a19d359324472
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-08-15 23:16:44 +0300

    mae synt cn tpe

--- experiments/synthetic/constant_noise/mae_based/mae_based_synthetic_const_noise.py
+++ experiments/synthetic/constant_noise/mae_based/mae_based_synthetic_const_noise.py
@@ -78,39 +78,16 @@ def bounded_loss(y_pred, y):
 
 """### Grid Search"""
 
-train_and_evaluate_net = train_and_evaluate_mae_mse(ROOT_BOUNDED_MAE + '/' + CHECKPOINT_BOUNDED_MAE, lambda: bounded_loss, plot = False, log = False)
+train_and_evaluate_net = train_and_evaluate_mae_mse(
+    dataset_train, dataset_val, bound_min, bound_max, ROOT_BOUNDED_MAE + '/' + CHECKPOINT_BOUNDED_MAE, lambda: bounded_loss, plot = False, log = False)
 
-def train_once_mae_cens_NO_trunc():
-    conf = {
-        'max_lr': 3e-2,
-        'epochs': 10,
-        'batch': 100,
-        'pct_start': 0.3,
-        'anneal_strategy': 'linear',
-        'base_momentum': 0.85,
-        'max_momentum': 0.95,
-        'div_factor': 3,
-        'final_div_factor': 1e4,
-        'weight_decay': 0
-    }
-    train_and_evaluate_net(dataset_train, dataset_val, bound_min, bound_max, conf)
-    plot_and_evaluate_model_mae_mse(bound_min, bound_max, x_mean, x_std, y_mean, y_std, dataset_val, dataset_test,
-                                    ROOT_BOUNDED_MAE, CHECKPOINT_BOUNDED_MAE, lambda: bounded_loss, is_optimized= False)
+def tpe_opt_mae_cens_NO_trunc():
+    return tpe_opt_hyperparam(ROOT_BOUNDED_MAE, CHECKPOINT_BOUNDED_MAE, train_and_evaluate_net)
 
-def grid_search_mae_cens_NO_trunc():
-    grid_config = get_grid_search_space()
-    grid_best = grid_search(ROOT_BOUNDED_MAE, dataset_train, dataset_val, bound_min, bound_max,
-                            grid_config, train_and_evaluate_net, CHECKPOINT_BOUNDED_MAE, conf_validation = config_validation)
-    return grid_best
 
 def eval_mae_cens_NO_trunc():
     plot_and_evaluate_model_mae_mse(bound_min, bound_max, x_mean, x_std, y_mean, y_std, dataset_val, dataset_test,
                                     ROOT_BOUNDED_MAE, CHECKPOINT_BOUNDED_MAE, lambda: bounded_loss, is_optimized= True)
-    grid_results = t.load(ROOT_BOUNDED_MAE + '/' + GRID_RESULTS_FILE)
-    best_config = grid_results['best']
-    best_metrics = grid_results[str(best_config)]
-    print(best_config)
-    print(best_metrics)
 
 def plot_mae_cens_NO_trunc():
     checkpoint = t.load(f'{ROOT_BOUNDED_MAE}/grid {CHECKPOINT_BOUNDED_MAE}.tar')
@@ -129,40 +106,17 @@ def below_zero_mae_penalty(y_pred):
 def bounded_loss_with_penalty(y_pred, y):
   return bounded_loss(y_pred, y) + below_zero_mae_penalty(y_pred)
 
-train_and_evaluate_net = train_and_evaluate_mae_mse(ROOT_BOUNDED_MAE_WITH_PENALTY + '/' + CHECKPOINT_BOUNDED_MAE_WITH_PENALTY,
-                                                    lambda: bounded_loss_with_penalty, plot = False, log = False)
-
-def train_once_mae_cens_WITH_trunc():
-    conf = {
-        'max_lr': 2e-2,
-        'epochs': 10,
-        'batch': 100,
-        'pct_start': 0.3,
-        'anneal_strategy': 'linear',
-        'base_momentum': 0.85,
-        'max_momentum': 0.95,
-        'div_factor': 2,
-        'final_div_factor': 1e4,
-        'weight_decay': 0
-    }
-    train_and_evaluate_net(dataset_train, dataset_val, bound_min, bound_max, conf)
-    plot_and_evaluate_model_mae_mse(bound_min, bound_max, x_mean, x_std, y_mean, y_std, dataset_val, dataset_test, ROOT_BOUNDED_MAE_WITH_PENALTY,
-                                    CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, lambda: bounded_loss_with_penalty, is_optimized= False)
+train_and_evaluate_net = train_and_evaluate_mae_mse(
+    dataset_train, dataset_val, bound_min, bound_max,
+    ROOT_BOUNDED_MAE_WITH_PENALTY + '/' + CHECKPOINT_BOUNDED_MAE_WITH_PENALTY,
+    lambda: bounded_loss_with_penalty, plot = False, log = False)
 
 def grid_search_mae_cens_WITH_trunc():
-    grid_config = get_grid_search_space()
-    grid_best = grid_search(ROOT_BOUNDED_MAE_WITH_PENALTY, dataset_train, dataset_val, bound_min, bound_max,
-                grid_config, train_and_evaluate_net, CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, conf_validation = config_validation)
-    return grid_best
+    return tpe_opt_hyperparam(ROOT_BOUNDED_MAE_WITH_PENALTY, CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, train_and_evaluate_net)
 
 def eval_mae_cens_WITH_trunc():
     plot_and_evaluate_model_mae_mse(bound_min, bound_max, x_mean, x_std, y_mean, y_std, dataset_val, dataset_test, ROOT_BOUNDED_MAE_WITH_PENALTY,
                                     CHECKPOINT_BOUNDED_MAE_WITH_PENALTY, lambda: bounded_loss_with_penalty, is_optimized= True)
-    grid_results = t.load(ROOT_BOUNDED_MAE_WITH_PENALTY + '/' + GRID_RESULTS_FILE)
-    best_config = grid_results['best']
-    best_metrics = grid_results[str(best_config)]
-    print(best_config)
-    print(best_metrics)
 
 def plot_mae_cens_WITH_trunc():
     checkpoint = t.load(f'{ROOT_BOUNDED_MAE_WITH_PENALTY}/grid {CHECKPOINT_BOUNDED_MAE_WITH_PENALTY}.tar')
