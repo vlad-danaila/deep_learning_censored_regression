adbde0ae6747b63fcb0be0940a890262cdbe455b
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-08-15 20:24:20 +0300

    tpe checkpoint saving

--- experiments/tpe_hyperparam_opt.py
+++ experiments/tpe_hyperparam_opt.py
@@ -1,3 +1,5 @@
+import math
+
 import optuna
 import torch as t
 import logging
@@ -11,6 +13,10 @@ from experiments.synthetic.plot import plot_epochs
 from experiments.train import train_network_mae_mse_gll, train_network_tobit_fixed_std, train_network_tobit_dyn_std
 from experiments.constants import NB_TRIALS, TPE_STARTUP_TRIALS, SEED
 from os.path import join
+import os
+
+PREVIOUS_BEST = 'PREVIOUS_BEST'
+CHECKPOINT = 'CHECKPOINT'
 
 def propose_conf(trial: optuna.trial.Trial):
     return {
@@ -26,15 +32,40 @@ def propose_conf(trial: optuna.trial.Trial):
         'weight_decay': 0
     }
 
-def tpe_opt_hyperparam(root_folder, checkpoint_name, train_callback):
+def logging_callback(study, frozen_trial):
+    previous_best_value = study.user_attrs.get("previous_best_value", None)
+    if previous_best_value != study.best_value:
+        study.set_user_attr("previous_best_value", study.best_value)
+        print(
+            "Trial {} finished with best value: {} and parameters: {}. ".format(
+                frozen_trial.number,
+                frozen_trial.value,
+                frozen_trial.params,
+            )
+        )
+
+def save_checkpoint_callback(study: optuna.study.Study, trial: optuna.trial.FrozenTrial):
+    prev_best = study.user_attrs.get(PREVIOUS_BEST)
+    checkpoint = study.user_attrs.get(CHECKPOINT)
+    checkpoint_file = checkpoint + '.tar'
+    best_checkpoint_file = checkpoint + ' best.tar'
+    if trial.value < prev_best:
+        study.set_user_attr(PREVIOUS_BEST, trial.value)
+        if os.path.exists(best_checkpoint_file):
+            os.remove(best_checkpoint_file)
+        os.rename(checkpoint_file, best_checkpoint_file)
+
+def tpe_opt_hyperparam(root_folder, checkpoint, train_callback):
     # TODO add prunner, don't forget to include in optuna.create_study(..., pruner = pruner) or just raise optuna.TrialPruned()
-    study_name = f'study {checkpoint_name}'
+    study_name = f'study {checkpoint}'
     sampler = optuna.samplers.TPESampler(multivariate = True, n_startup_trials = TPE_STARTUP_TRIALS, seed=SEED)
     study = optuna.create_study(sampler=sampler, study_name = study_name, direction = 'minimize',
-                                storage = f'sqlite:///{root_folder}/{checkpoint_name}.db', load_if_exists = True)
-    study.optimize(train_callback, n_trials = NB_TRIALS)
+                                storage = f'sqlite:///{root_folder}/{checkpoint}.db', load_if_exists = True)
+    study.set_user_attr(PREVIOUS_BEST, math.inf)
+    study.set_user_attr(CHECKPOINT, f'{root_folder}/{checkpoint}')
+    study.optimize(train_callback, n_trials = NB_TRIALS, callbacks=[save_checkpoint_callback])
     logging.info(study.best_params)
-    dump_json(study.best_params, f'{root_folder}/{checkpoint_name} hyperparam.json')
+    dump_json(study.best_params, f'{root_folder}/{checkpoint} hyperparam.json')
 
 def get_objective_fn_mae_mse(dataset_train, dataset_val, bound_min, bound_max, checkpoint, criterion, model_fn = DenseNetwork, plot = False, log = True):
     def objective_fn(trial: optuna.trial.Trial):
