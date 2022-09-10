e035b0858118779a87c8146a9ad8adea8d2c0ade
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-09-10 13:13:05 +0300

    synt heter tpe mae mse std dyn

--- experiments/synthetic/heteroscedastic/train_all.py
+++ experiments/synthetic/heteroscedastic/train_all.py
@@ -1,5 +1,5 @@
-from experiments.synthetic.heteroscedastic.mae_based.mae_based_synthetic_heter import eval_mae_simple, eval_mae_cens_NO_trunc, eval_mae_cens_WITH_trunc
-from experiments.synthetic.heteroscedastic.mse_based.mse_based_synthetic_heter import eval_mse_simple, eval_mse_cens_NO_trunc, eval_mse_cens_WITH_trunc
+from experiments.synthetic.heteroscedastic.mae_based.mae_based_synthetic_heter import tpe_opt_mae_simple, tpe_opt_mae_cens_NO_trunc, tpe_opt_mae_cens_WITH_trunc
+from experiments.synthetic.heteroscedastic.mse_based.mse_based_synthetic_heter import tpe_opt_mse_simple, tpe_opt_mse_cens_NO_trunc, tpe_opt_mse_cens_WITH_trunc
 # from experiments.synthetic.heteroscedastic.tobit_based.scaled_fixed_std.scaled_gll_synthetic_data import tpe_opt_gll_scaled
 # from experiments.synthetic.heteroscedastic.tobit_based.scaled_fixed_std.scaled_tobit_loss_synthetic_data import tpe_opt_deep_NO_trunc, tpe_opt_deep_WITH_trunc, \
 #     tpe_opt_lin_NO_trunc, tpe_opt_lin_WITH_trunc
@@ -10,20 +10,20 @@ from experiments.synthetic.heteroscedastic.tobit_based.scaled_dynamic_std.scaled
 from experiments.synthetic.heteroscedastic.tobit_based.reparam_dynamic_std.reparametrized_tobit_loss_dynamic_std_synthetic_heter_data import tpe_opt_deep_WITH_trunc_reparam_dyn_std
 
 # MAE Based
-print('-' * 20, 'eval_mae_simple', '-' * 20)
-eval_mae_simple()
-print('-' * 20, 'eval_mae_cens_NO_trunc', '-' * 20)
-eval_mae_cens_NO_trunc()
-print('-' * 20, 'eval_mae_cens_WITH_trunc', '-' * 20)
-eval_mae_cens_WITH_trunc()
+print('-' * 20, 'tpe_opt_mae_simple', '-' * 20)
+tpe_opt_mae_simple()
+print('-' * 20, 'tpe_opt_mae_cens_NO_trunc', '-' * 20)
+tpe_opt_mae_cens_NO_trunc()
+print('-' * 20, 'tpe_opt_mae_cens_WITH_trunc', '-' * 20)
+tpe_opt_mae_cens_WITH_trunc()
 
 # MSE Based
-print('-' * 20, 'eval_mse_simple', '-' * 20)
-eval_mse_simple()
-print('-' * 20, 'eval_mse_cens_NO_trunc', '-' * 20)
-eval_mse_cens_NO_trunc()
-print('-' * 20, 'eval_mse_cens_WITH_trunc', '-' * 20)
-eval_mse_cens_WITH_trunc()
+print('-' * 20, 'tpe_opt_mse_simple', '-' * 20)
+tpe_opt_mse_simple()
+print('-' * 20, 'tpe_opt_mse_cens_NO_trunc', '-' * 20)
+tpe_opt_mse_cens_NO_trunc()
+print('-' * 20, 'tpe_opt_mse_cens_WITH_trunc', '-' * 20)
+tpe_opt_mse_cens_WITH_trunc()
 
 # Tobit
 
