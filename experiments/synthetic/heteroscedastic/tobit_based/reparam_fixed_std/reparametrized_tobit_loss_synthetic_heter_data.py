e2e65a3b035283eed5809ccaa58baf52351f84e0
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-09-14 22:01:59 +0300

    cleanup

--- experiments/synthetic/heteroscedastic/tobit_based/reparam_fixed_std/reparametrized_tobit_loss_synthetic_heter_data.py
+++ experiments/synthetic/heteroscedastic/tobit_based/reparam_fixed_std/reparametrized_tobit_loss_synthetic_heter_data.py
@@ -2,9 +2,7 @@ from experiments.constants import GRID_RESULTS_FILE
 from experiments.synthetic.constants import *
 from experiments.util import set_random_seed
 from experiments.synthetic.heteroscedastic.dataset import *
-from experiments.grid_search import grid_search, config_validation, get_grid_search_space
 from experiments.synthetic.eval_optimized import plot_and_evaluate_model_tobit_fixed_std
-from experiments.grid_train import train_and_evaluate_tobit_fixed_std
 from deep_tobit.util import normalize, distinguish_censored_versus_observed_data
 from experiments.synthetic.models import DenseNetwork
 from experiments.synthetic.eval_optimized import plot_dataset_and_net
