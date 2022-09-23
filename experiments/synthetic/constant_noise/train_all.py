c41ce053dd9abe1c53a71485c2ce0288c25991b5
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-09-23 11:54:13 +0300

    fix

--- experiments/synthetic/constant_noise/train_all.py
+++ experiments/synthetic/constant_noise/train_all.py
@@ -1,5 +1,5 @@
-from experiments.synthetic.constant_noise.mae_based.mae_based_synthetic_const_noise import eval_mae_simple, eval_mae_cens_NO_trunc, eval_mae_cens_WITH_trunc
-from experiments.synthetic.constant_noise.mse_based.mse_based_synthetic_const_noise import eval_mse_simple, eval_mse_cens_NO_trunc, eval_mse_cens_WITH_trunc
+from experiments.synthetic.constant_noise.mae_based.mae_based_synthetic_const_noise import tpe_opt_mae_simple, tpe_opt_mae_cens_NO_trunc, tpe_opt_mae_cens_WITH_trunc
+from experiments.synthetic.constant_noise.mse_based.mse_based_synthetic_const_noise import tpe_opt_mse_simple, tpe_opt_mse_cens_NO_trunc, tpe_opt_mse_cens_WITH_trunc
 from experiments.synthetic.constant_noise.tobit_based.scaled_fixed_std.scaled_gll_synthetic_data import tpe_opt_gll_scaled
 from experiments.synthetic.constant_noise.tobit_based.scaled_fixed_std.scaled_tobit_loss_synthetic_data import tpe_opt_deep_NO_trunc, tpe_opt_deep_WITH_trunc, \
     tpe_opt_lin_NO_trunc, tpe_opt_lin_WITH_trunc
@@ -10,14 +10,14 @@ from experiments.synthetic.constant_noise.tobit_based.scaled_dynamic_std.scaled_
 from experiments.synthetic.constant_noise.tobit_based.reparam_dynamic_std.reparametrized_tobit_loss_dynamic_std_synthetic_const_noise_data import tpe_opt_deep_WITH_trunc_reparam_dyn_std
 
 # MAE Based
-eval_mae_simple()
-eval_mae_cens_NO_trunc()
-eval_mae_cens_WITH_trunc()
+tpe_opt_mae_simple()
+tpe_opt_mae_cens_NO_trunc()
+tpe_opt_mae_cens_WITH_trunc()
 
 # MSE Based
-eval_mse_simple()
-eval_mse_cens_NO_trunc()
-eval_mse_cens_WITH_trunc()
+tpe_opt_mse_simple()
+tpe_opt_mse_cens_NO_trunc()
+tpe_opt_mse_cens_WITH_trunc()
 
 # Tobit
 
