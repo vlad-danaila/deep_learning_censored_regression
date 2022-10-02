7a1fd68d0c5c502884f3457ee1589e5406604cbe
Vlad Danaila <vlad.danaila@yahoo.com>
2022-10-02 16:33:00 +0300

    distributions variations

--- experiments/synthetic/dynamic/plot_beta_distributions.py
+++ experiments/synthetic/dynamic/plot_beta_distributions.py
@@ -5,12 +5,20 @@ from experiments.util import TruncatedBetaDistributionConfig, name_from_distribu
 from experiments.synthetic.constants import *
 from experiments.synthetic.plot import plot_beta, plot_dataset
 
-def plot_distribution(alpha, beta):
+def plot_distribution(is_heteroscedastic, alpha, beta):
     config = TruncatedBetaDistributionConfig(
-        censor_low_bound = CENSOR_LOW_BOUND, censor_high_bound = CENSOR_HIGH_BOUND, alpha = alpha, beta = beta, is_heteroscedastic = False
+        censor_low_bound = CENSOR_LOW_BOUND, censor_high_bound = CENSOR_HIGH_BOUND,
+        alpha = alpha, beta = beta,
+        is_heteroscedastic = is_heteroscedastic
     )
     dataset_train, dataset_val, dataset_test, bound_min, bound_max, zero_normalized, x_mean, x_std, y_mean, y_std = get_experiment_data(config)
     plot_beta(x_mean, x_std, y_mean, y_std, a=alpha, b=beta)
     plot_dataset(dataset_train)
 
-plot_distribution(1, 4)
+def get_distirbution_variations():
+    return [
+        (False, 1, 4), (False, 2.5, 4), (False, 4, 4), (False, 4, 2.5), (False, 4, 1),
+        (True, 1, 4), (True, 2.5, 4), (True, 4, 4), (True, 4, 2.5), (True, 4, 1)
+    ]
+
+plot_distribution(True, 2.5, 4)
