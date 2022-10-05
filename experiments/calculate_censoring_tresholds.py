fcd563753e1baf72898ff84751f0b4c50cd00ee2
Vlad Rares Danaila <vlad.danaila@ro.ibm.com>
2022-10-05 20:38:05 +0300

    calculating censoring tresholds

--- experiments/calculate_censoring_tresholds.py
+++ experiments/calculate_censoring_tresholds.py
@@ -6,9 +6,10 @@ from experiments.util import TruncatedBetaDistributionConfig, name_from_distribu
 from experiments.synthetic.constants import *
 from experiments.synthetic.plot import plot_beta, plot_dataset as plot_dataset_synt
 from experiments.real.pm25.dataset import extract_features as extract_features_pm25, load_dataframe as load_dataframe_pm25, \
-    numeric_features_column_names, x_numeric_fetures_mean, x_numeric_fetures_std, pca
+    numeric_features_column_names as numeric_features_pm25, x_numeric_fetures_mean as x_mean_pm25, x_numeric_fetures_std as x_std_pm25, pca
 from experiments.real.pm25.plot import plot_full_dataset as plot_full_dataset_pm25
-from experiments.real.bike_sharing.dataset import extract_features as extract_features_bike, load_dataframe as load_dataframe_bike
+from experiments.real.bike_sharing.dataset import extract_features as extract_features_bike, load_dataframe as load_dataframe_bike, y_variable_label, \
+    numeric_features_column_names as numeric_features_bike, x_numeric_fetures_mean as x_mean_bike, x_numeric_fetures_std as x_std_bike
 from experiments.real.bike_sharing.plot import plot_full_dataset as plot_full_dataset_bike
 import matplotlib.pyplot as plt
 import numpy as np
@@ -45,30 +46,54 @@ plot_tresholds_for_synthetic(is_heteroscedastic, alpha, beta, percentile_low, pe
 
 
 # PM25
-# TODO
-df = load_dataframe_pm25()
-one_hot = sk.preprocessing.OneHotEncoder(sparse = False)
-month_one_hot = one_hot.fit_transform(np.expand_dims(df['month'].values, 1))
-hour_one_hot = one_hot.fit_transform(np.expand_dims(df['hour'].values, 1))
-combined_wind_direction_one_hot = one_hot.fit_transform(np.expand_dims(df['cbwd'].values, 1))
-numeric_fetures = df[numeric_features_column_names].values
-numeric_fetures = normalize(numeric_fetures, x_numeric_fetures_mean, x_numeric_fetures_std)
-x = np.hstack((month_one_hot, hour_one_hot, combined_wind_direction_one_hot, numeric_fetures))
-x = pca(x)
-y = df['pm2.5'].values
-plt.scatter(x, y, s = .1)
+def plot_tresholds_for_pm25(percentile_low, percentile_high):
+    df = load_dataframe_pm25()
+    one_hot = sk.preprocessing.OneHotEncoder(sparse = False)
+    month_one_hot = one_hot.fit_transform(np.expand_dims(df['month'].values, 1))
+    hour_one_hot = one_hot.fit_transform(np.expand_dims(df['hour'].values, 1))
+    combined_wind_direction_one_hot = one_hot.fit_transform(np.expand_dims(df['cbwd'].values, 1))
+    numeric_fetures = df[numeric_features_pm25].values
+    numeric_fetures = normalize(numeric_fetures, x_mean_pm25, x_std_pm25)
+    x = np.hstack((month_one_hot, hour_one_hot, combined_wind_direction_one_hot, numeric_fetures))
+    x = pca(x)
+    y = df['pm2.5'].values
+    bound_min, bound_max = np.percentile(y, [percentile_low, percentile_high])
+    print(bound_min, bound_max)
+    x_interval = [min(x), max(x)]
+    plt.scatter(x, y, s = .1)
+    plt.plot(x_interval, [bound_min] * 2, color = 'red', linewidth=.5)
+    plt.plot(x_interval, [bound_max] * 2, color = 'red', linewidth=.5)
+
+plot_tresholds_for_pm25(20, 80)
 
 
 
-
-x, y = extract_features_pm25(df)
-y = y.reshape(y.shape[0])
-bound_min, bound_max = np.percentile(y, [20, 80])
-plot_full_dataset_pm25(df, bound_min=bound_min, bound_max=bound_max)
 
 # Bike
-df = load_dataframe_bike()
-x, y = extract_features_bike(df)
-y = y.reshape(y.shape[0])
-bound_min, bound_max = np.percentile(y, [20, 80])
-plot_full_dataset_bike(df, bound_min=bound_min, bound_max=bound_max)
+def plot_tresholds_for_bike(percentile_low, percentile_high):
+    df = load_dataframe_bike()
+    one_hot = sk.preprocessing.OneHotEncoder(sparse = False)
+    hour_one_hot = one_hot.fit_transform(np.expand_dims(df['Hour'].values, 1))
+    seansons_one_hot = one_hot.fit_transform(np.expand_dims(df['Seasons'].values, 1))
+    holiday_one_hot = one_hot.fit_transform(np.expand_dims(df['Holiday'].values, 1))
+    functioning_day_one_hot = one_hot.fit_transform(np.expand_dims(df['Functioning Day'].values, 1))
+    # extract the numeric variables
+    numeric_fetures = df[numeric_features_bike].values
+    numeric_fetures = normalize(numeric_fetures, x_mean_bike, x_std_bike)
+    # unite all features
+    x = np.hstack((hour_one_hot, seansons_one_hot, holiday_one_hot, functioning_day_one_hot, numeric_fetures))
+    x = pca(x)
+    # extract the results
+    y = df[y_variable_label].values
+    bound_min, bound_max = np.percentile(y, [percentile_low, percentile_high])
+    print(bound_min, bound_max)
+    x_interval = [min(x), max(x)]
+    plt.scatter(x, y, s = .1)
+    plt.plot(x_interval, [bound_min] * 2, color = 'red', linewidth=.5)
+    plt.plot(x_interval, [bound_max] * 2, color = 'red', linewidth=.5)
+
+plot_tresholds_for_bike(20, 80)
+
+
+
+
